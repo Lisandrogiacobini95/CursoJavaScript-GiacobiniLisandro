bcbfabaeab43b91325637e06064d0a70b9ede92d
lisandrogiacobini <lisandrogiacobini95@hotmail.com>
2024-05-27 16:53:50 -0300

    Se avanzo en la seccion de pasar lista

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const alumnos = JSON.parse(localStorage.getItem("alumnos")) || []
 
 /*LLAMO A ESTA FUNCION CUANDO DESEO CARGAR LOS DATOS DE UN NUEVO ALUMNO*/
 const cargar_datos_alumno = (nombre, apellido, grado) => {
-   
+
     const alumno = {
         nombre: nombre.toUpperCase(),
         apellido: apellido.toUpperCase(),
@@ -12,7 +12,7 @@ const cargar_datos_alumno = (nombre, apellido, grado) => {
         presente: 0
     }
     alumnos.push(alumno)
-    localStorage.setItem("alumnos",JSON.stringify(alumnos))
+    localStorage.setItem("alumnos", JSON.stringify(alumnos))
     alert("El alumno fue cargado exitosamente")
 }
 
@@ -21,60 +21,60 @@ const buscar_datos_alumno = (apellido) => {
     alumnos.forEach(alumno => {
         if (alumno.apellido === apellido.toUpperCase()) {
             alert("NOMBRE: " + alumno.nombre + " APELLIDO: " + alumno.apellido + " GRADO: " + alumno.grado)
-        }})
-    
+        }
+    })
+
 }
 
 /*LLAMO A ESTA FUNCION CUANDO DESEO PASAR ASISTENCIA*/
-const pasar_asistencia_alumnos = () => {
-    console.log("HOLAA")
-    alumnos.forEach(alumno =>{
-        const app = document.getElementById("pasar_lista-edit")
-        const element = document.createElement("div")
-        element.className = "tarjeta"
-        element.innerHTML = `
-                                <input type="text"  class="input" value="${alumno.nombre}">
-                                <textarea type="text"  class="input">${alumno.contenido}</textarea>
-                                <button class="btn btn-actualizar">Actualizar</button>
-                                <button class="btn btn-borrar">Borrar</button>
+const pasar_asistencia_alumnos = (alumno) => {
+    const app = document.getElementById("app")
+    const element = document.createElement("div")
+    element.className = "card pasar_lista-edit"
+    element.style = "width: 18rem"
+    element.innerHTML = `
+                        <img src="../assets/imagen-alumnos.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title"> ${alumno.nombre} ${alumno.apellido}</h5>
+                            <button type="button" id="btn-presente-${alumno.apellido}" class="btn btn-dark">Presente</button>
+                            <button type="button" id="btn-ausente-${alumno.apellido}" class="btn btn-dark">Ausente</button>
+                        </div>
         `
-        if(app){
-            app.append(element)
-        }
-    })
-    
+    app.append(element)
 }
 
-const principal = () => {
+const main = () => {
     //CARGARMOS LA FUNCION A EJERCUTAR AL PRESIONAR EL BOTON CARGAR ALUMNO
     const btnCargarAlumno = document.getElementById("btn-cargarAlumno")
-    if(btnCargarAlumno){
-        btnCargarAlumno.addEventListener("click",()=>{
+    if (btnCargarAlumno) {
+        btnCargarAlumno.addEventListener("click", () => {
             const nombreAlumnoCargar = document.getElementById("nombreAlumno-cargar")
             const apellidoAlumnoCargar = document.getElementById("apellidoAlumno-cargar")
             const gradoAlumnoCargar = document.getElementById("gradoAlumno-cargar")
-            cargar_datos_alumno(nombreAlumnoCargar.value,apellidoAlumnoCargar.value,gradoAlumnoCargar.value)
+            cargar_datos_alumno(nombreAlumnoCargar.value, apellidoAlumnoCargar.value, gradoAlumnoCargar.value)
             nombreAlumnoCargar.value = ""
             apellidoAlumnoCargar.value = ""
             gradoAlumnoCargar.value = 0
             console.log(alumnos)
         })
     }
-    
+
     //CARGARMOS LA FUNCION A EJERCUTAR AL PRESIONAR EL BOTON BUSCAR ALUMNO
     const btnBuscarAlumno = document.getElementById("btn-buscarAlumno")
-    if(btnBuscarAlumno){
-        btnBuscarAlumno.addEventListener("click",()=>{
+    if (btnBuscarAlumno) {
+        btnBuscarAlumno.addEventListener("click", () => {
             const apellidoAlumnoBuscar = document.getElementById("apellidoAlumno-buscar")
             buscar_datos_alumno(apellidoAlumnoBuscar.value)
             apellidoAlumnoBuscar.value = ""
         })
     }
 
-    const btnPasarLista = document.getElementById("btn-pasarLista-index")
-    if(btnPasarLista){
-        btnPasarLista.addEventListener("click",pasar_asistencia_alumnos())
+    const btnPresente = document.getElementById("btn-volverPasarLista")
+    if (btnPresente) {
+        alumnos.forEach(alumno => {
+            pasar_asistencia_alumnos(alumno)
+        })
     }
 }
 
-principal()
+main()
