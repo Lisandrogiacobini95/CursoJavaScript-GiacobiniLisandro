b14fae926caef98013e3e66ca7c93a664952c487
lisandrogiacobini <lisandrogiacobini95@hotmail.com>
2024-05-28 17:34:01 -0300

    Se avanzo en el codigo de la funcion buscar alumnos

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const alumnos = JSON.parse(localStorage.getItem("alumnos")) || []
 const cargar_datos_alumno = (nombre, apellido, grado) => {
 
     const alumno = {
+        id:crypto.randomUUID(),
         nombre: nombre.toUpperCase(),
         apellido: apellido.toUpperCase(),
         grado,
@@ -18,17 +19,41 @@ const cargar_datos_alumno = (nombre, apellido, grado) => {
 
 /*LLAMO A ESTA FUNCION CUANDO DESEO BUSCAR LOS DATOS DE ALGUN ALUMNO EN PARTICULAR */
 const buscar_datos_alumno = (apellido) => {
+    /*LIMPIO LA PAGINA DE BUSQUEDAS ANTERIORES*/
+    const appBuscar = document.getElementsByClassName("resultado-busqueda")[0];
+        while (appBuscar.firstChild) {
+            appBuscar.removeChild(appBuscar.firstChild);
+            }
+
+    /* CREO UNA VARIABLE PARA UTILIZAR COMO FLAG POR SI NO SE ENCUENTRAN RESULTADOS*/
+    let alumnoEncontrado = 0
     alumnos.forEach(alumno => {
         if (alumno.apellido === apellido.toUpperCase()) {
-            alert("NOMBRE: " + alumno.nombre + " APELLIDO: " + alumno.apellido + " GRADO: " + alumno.grado)
+            const appBuscar = document.getElementsByClassName("resultado-busqueda")[0]
+            const element = document.createElement("div")
+            element.className = "card"
+            element.style = "width: 18rem"
+            element.innerHTML = `
+                                <img src="../assets/imagen-alumnos.jpg" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title"> ${alumno.nombre} ${alumno.apellido}</h5>
+                                    <p class="card-text">El alumno va a ${alumno.grado} grado</p>
+                                    <p class="card-text">ID: ${alumno.id}</p>
+                                </div>
+                `
+            appBuscar.append(element)
+            alumnoEncontrado = 1
         }
     })
+    if(alumnoEncontrado == 0){
+        alert("No se encuentra ningun alumno con ese apellido en nuestra base de datos")
+    }
 
 }
 
 /*LLAMO A ESTA FUNCION CUANDO DESEO PASAR ASISTENCIA*/
 const pasar_asistencia_alumnos = (alumno) => {
-    const app = document.getElementById("app")
+    const appPasarLista = document.getElementById("app-pasarLista")
     const element = document.createElement("div")
     element.className = "card pasar_lista-edit"
     element.style = "width: 18rem"
@@ -36,11 +61,12 @@ const pasar_asistencia_alumnos = (alumno) => {
                         <img src="../assets/imagen-alumnos.jpg" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title"> ${alumno.nombre} ${alumno.apellido}</h5>
-                            <button type="button" id="btn-presente-${alumno.apellido}" class="btn btn-dark">Presente</button>
-                            <button type="button" id="btn-ausente-${alumno.apellido}" class="btn btn-dark">Ausente</button>
+                            <p class="card-text">El alumno va a ${alumno.grado} grado</p>
+                            <button type="button" id="btn-presente-${alumno.id}" class="btn btn-dark">Presente</button>
+                            <button type="button" id="btn-ausente-${alumno.id}" class="btn btn-dark">Ausente</button>
                         </div>
         `
-    app.append(element)
+    appPasarLista.append(element)
 }
 
 const main = () => {
